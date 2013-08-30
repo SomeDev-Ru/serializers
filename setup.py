317ad47e472c49ba0bb5aec09f803bbd03fce67a
Alexander Malev <alexander-malev@ya.ru>
2013-08-30 16:06:15 +0400

    +setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='serializers',
+    version='0.1',
+    package_dir={'serializers': ''},
+    packages=['serializers'],
+    author='Alexander Malev',
+    author_email='malev@somedev.ru',
+    license='BSD',
+    url='https://github.com/SomeDev-Ru/serializers.git',
+)
